9e423ea1a925965651a46d6468c72f9a55302e7a
Yakouttkadiri <57764694+Yakouttkadiri@users.noreply.github.com>
2022-06-14 22:22:05 +0200

    Update tp_devops.py

--- tp_devops.py
+++ tp_devops.py
@@ -3,10 +3,10 @@ import os
 import json
 
 
-cle_api = "240aa650f4db4e154a07d0459c30a347"
-lat = "39.571625"
-lon = "2.650544"
-url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, cle_api)
+APIKEY = "240aa650f4db4e154a07d0459c30a347"
+LAT = "39.571625"
+LONG = "2.650544"
+url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric" % (LAT,LONG, APIKEY)
 response = requests.get(url)
 data = json.loads(response.text)
 print(data)
